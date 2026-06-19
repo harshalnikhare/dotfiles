a45015eca46f27ed56439d57673dc4ae8165b10c
harshaln <harshaln369@gmail.com>
2026-06-19 08:53:02 +0530

    moved comments in their respective places

--- modules/services.nix
+++ modules/services.nix
@@ -38,6 +38,11 @@ _:
 
     # Enable touchpad support (enabled default in most desktopManager).
     # services.xserver.libinput.enable = true;
+    #
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
 
   };
 
