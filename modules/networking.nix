5912001b9cbc89ca379d28fe30dd78e254c0249e
harshaln <harshaln369@gmail.com>
2026-07-05 20:08:45 +0530

    enable firewall

--- modules/networking.nix
+++ modules/networking.nix
@@ -4,7 +4,7 @@ _:
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -14,6 +14,6 @@ _:
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
 }
